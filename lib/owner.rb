a0734a7f79a9ba61c11a34daf14d5aeef2d2eb5a
anil gurung <anil_grg@hotmail.com>
2020-01-10 10:12:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_reader :name,:species
+  attr_accessor :count
+  @@all = []
+  
+  def initialize(name,species = "human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+  
+  def say_species
+    @say_species = p "I am a human."
+  end
+  
+  def self.all
+  @@all
+  end
+  
+ # binding.pry
+ def self.count
+ self.all.count
+ end
+ 
+ def self.reset_all
+   self.all.clear
+ end
 end
+
